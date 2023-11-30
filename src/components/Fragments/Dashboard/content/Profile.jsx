74864b089411392b6dfdfd26f3db3d742fce8834
rizkydwiananto <rizkydwiananto@gmail.com>
2023-11-30 21:58:26 +0700

    update coding bareng 30 November 2023

--- src/components/Fragments/Dashboard/content/Profile.jsx
+++ src/components/Fragments/Dashboard/content/Profile.jsx
@@ -8,16 +8,16 @@ export default function Profile() {
       <div className="flex w-full justify-around items-center">
         <div className="avatar">
           <div className="w-52 rounded-full shadow-md">
-            {user && <img src={user.image} alt="profile image" />}
+            {user && <img src={user.foto_pekerja} alt="profile image" />}
           </div>
         </div>
         <div>
           {user && (
             <p className="text-4xl font-bold text-orange-500 mb-2">
-              {user.firstName + " " + user.lastName}{" "}
+              {user.nama_depan + " " + user.nama_belakang}{" "}
             </p>
           )}
-          {user && <p>{user.gender} </p>}
+          {user && <p>{user.jns_kel} </p>}
         </div>
         <div>
           <button className="btn px-10 bg-orange-500 text-slate-50">
@@ -28,14 +28,7 @@ export default function Profile() {
 
       <div className="mt-20">
         <h2 className="text-2xl font-bold text-orange-500">Tentang Saya</h2>
-        <p className="text-gray-500">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
-          aspernatur qui deleniti neque voluptate placeat! Ullam impedit aliquid
-          suscipit consequuntur dolorum. Quam, reiciendis fuga. Quasi dolores
-          nihil veniam minima facilis reiciendis et quod natus, quis sed rerum,
-          accusantium blanditiis! Dolore, adipisci. Nihil deserunt dicta quasi
-          excepturi omnis rerum debitis esse.
-        </p>
+        <p className="text-gray-500">{user.detail_tentang_saya}</p>
       </div>
 
       <div>
@@ -49,10 +42,14 @@ export default function Profile() {
             <p>Domisili</p>
           </div>
           <div className="space-y-2">
-            {user && <p>{user.firstName + " " + user.lastName}</p>}
-            {user && <p>{user.firstName}</p>}
-            <p>01 januari 1990</p>
+            {user && <p>{user.nama_depan + " " + user.nama_belakang}</p>}
+            {user && <p>{user.nama_depan}</p>}
+            <p>
+              {user.tempat_lahir}, {user.tanggal_lahir}
+            </p>
             {user && <p>{user.gender}</p>}
+            <p>{user.nomor_hp}</p>
+            <p>{user.domisili}</p>
           </div>
         </div>
       </div>
